a8895ab30d67adee501d6e8eff9938b6dede0712
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-01-09 11:13:30 -0400

    new codigo

--- javachess/src/entities/App.java
+++ javachess/src/entities/App.java
@@ -1,36 +1,55 @@
 package entities;
 
+import java.util.Date;
 import java.text.ParseException;
-import java.util.InputMismatchException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class App {
-    
-        public static void main(String[] args) throws ParseException {
-            Locale.setDefault(Locale.US);
-            Scanner sc = new Scanner(System.in);
 
-            try{
-            String[] vect = sc.nextLine().split(" ");
-            int position = sc.nextInt();
-            System.out.println(vect[position]);
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            System.out.println("Room Number: ");
+            int number = sc.nextInt();
+            System.out.println("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.println("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
+
+            if (!checkOut.after(checkIn)) {
+                System.out.println("Error: Check-out date must be after check-in date.");
+                return;
             }
-            catch(ArrayIndexOutOfBoundsException e){
 
-                System.out.println("Invalid position");
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.println("Check-in date (dd/MM/yyyy): ");
+            Date newCheckIn = sdf.parse(sc.next());
+            System.out.println("Check-out date (dd/MM/yyyy): ");
+            Date newCheckOut = sdf.parse(sc.next());
+
+            if (!newCheckOut.after(newCheckIn)) {
+                System.out.println("Error: Check-out date must be after check-in date.");
+                return;
             }
-                catch(InputMismatchException a){
-                    System.out.println("Name invalid");
-                }
 
+            reservation.updateDates(newCheckIn, newCheckOut);
+            System.out.println("Updated Reservation: " + reservation);
 
-
+        } catch (ParseException e) {
+            System.out.println("Invalid date format. Please use dd/MM/yyyy.");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid data.");
+        } finally {
             sc.close();
-        
-
- 
+        }
     }
-
 }
