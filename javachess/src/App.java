8bbc56dbc188a49336d9f419de4ddf392f106c8b
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-01-18 23:15:42 -0400

    NEW CODIGO

--- javachess/src/App.java
+++ javachess/src/App.java
@@ -11,28 +11,7 @@ public class App {
     
             Locale.setDefault(Locale.US);
             Scanner sc = new Scanner(System.in);
-            List<Shape> myShapes = new ArrayList<>();
-            myShapes.add(new Retangle(3.0, 2.0));
-            myShapes.add(new Circle(2.0));
 
-            List<Circle> myCircles = new ArrayList<>();
-            myCircles.add(new Circle(2.0));
-            myCircles.add(new Circle(3.0));
-
-
-            System.out.println("Total area: " + totalArea(myCircles));
-            sc.close();  
-
-    }
-    public static double totalArea(List<? extends Shape> list){
-    
-            double sum = 0.0;
-            for(Shape s: list){
-                sum += s.area();
-            }
-
-            return sum;
-            
     }
     
 }
