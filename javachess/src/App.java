9baf11c3bc153f14973b07bafe9b5790ccdfbaa7
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-02-05 15:29:11 -0400

    seller entities class

--- javachess/src/App.java
+++ javachess/src/App.java
@@ -1,14 +1,19 @@
 //import java.sql.Connection;
+import java.util.Date;
 import java.util.Locale;
 
 import modelEntities.Departament;
+import modelEntities.Seller;
 
 public class App {
         public static void main(String[] args){
             Locale.setDefault(Locale.US);
 
-           Departament obj = new Departament(1, "Nice");
+           Departament obj = new Departament(1, "Game");
            System.out.println(obj);
+
+           Seller seller = new Seller(2, "Valhalla", "valhalla@nice.com", new Date(), 3000.0, obj );
+           System.out.println(seller);
             
             
 
